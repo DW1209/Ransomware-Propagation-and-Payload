d1f9f468f0e5299b385aba96deb29e1dc0a137f6
dw1209 <daniel20001209@gmail.com>
2023-05-01 00:56:12 +0800

    change code style

--- attack_server.py
+++ attack_server.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
-import time
+
 import sys
+import time
+
 
 def get_parameters():
     if len(sys.argv) != 2:
-        print("usage: ./attack_server <Attacker port>")
+        print(f"usage: {__file__} <Attacker port>")
         exit(-1)
-
     return sys.argv[1]
 
 
